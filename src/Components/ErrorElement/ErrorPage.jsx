79c5b44fc39f0b64415cd8f28df0cf4f90182971
hasanalam24 <hasanalam365@gmail.com>
2024-04-30 11:31:53 +0600

    error page

--- src/Components/ErrorElement/ErrorPage.jsx
+++ src/Components/ErrorElement/ErrorPage.jsx
@@ -10,13 +10,18 @@ const ErrorPage = () => {
     }
 
     return (
-        <div>
+        <div className="relative bg-blue-200 h-[600px]">
             <Helmet>
                 <title>EP || Error</title>
 
             </Helmet>
-            <h1>Error Page 404</h1>
-            <button className="btn btn-secondary" onClick={handleBack}>Go Back</button>
+            <div className=" absolute mx-auto top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-white w-1/2 h-[300px]">
+                <h1 className=" mb-5 text-center mt-10 text-6xl font-medium">404</h1>
+                <p className="text-center mb-8">Page Not Found</p>
+                <div className="text-center">
+                    <button className="btn btn-secondary" onClick={handleBack}>Go Back</button>
+                </div>
+            </div>
         </div>
     );
 };
